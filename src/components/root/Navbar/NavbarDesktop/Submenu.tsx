78d465df1647d447f6a9e2ddd6f89ac012542c45
LilAngelBruh <angel117tutu@gmail.com>
2024-12-12 16:24:43 -0700

    fix(website): :bug: El navbar desktop no funciona bien

--- src/components/root/Navbar/NavbarDesktop/Submenu.tsx
+++ src/components/root/Navbar/NavbarDesktop/Submenu.tsx
@@ -9,7 +9,7 @@ export default function Submenu({ subMenu }: props) {
   return (
     <ul
       className="bg-white absolute top-full left-0 w-max mt-2 p-2 rounded-lg shadow-md opacity-0 invisible 
-        group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10"
+        group-hover:opacity-100 group-hover:visible transition-all duration-300 z-10 group-last:left-auto group-last:right-0"
     >
       {subMenu.map((item, idx) => (
         <li key={`${item.title}-${idx}`} className="w-full">
